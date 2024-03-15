c1c03678c4f700b00e7b14b64a54ea470e91cbb3
Vaimiti Utia <mitiii.utia.percheron@gmail.com>
2024-03-14 14:25:33 -1000

    Ceci est la Version 2

---
+++ src/pack1/Main.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("C'est la Version 1");
+		System.out.println("C'est la Version 2");
+
+	}
+
+}
